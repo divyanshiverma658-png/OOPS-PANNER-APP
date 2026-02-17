c16c1a9be31f8fd40bf7f38c16cf13157ee9f938
divyanshiverma658-png <divyanshiverma658.com>
2026-02-17 09:23:22 +0530

    Initial commit

---
+++ OOPSBannerAPP1.java
@@ -0,0 +1,16 @@
+
+    public class OOPSBannerAPP1 {
+   public OOPSBannerAPP1() {
+   }
+
+   public static void main(String[] var0) {
+      System.out.println("*****   *****   ******  ******* ");
+      System.out.println(" ** **   ** **   ** **   ****");
+      System.out.println(" ** **   ** **   ** **   ** ");
+      System.out.println(" ** **   ** **   ******  ******* ");
+      System.out.println(" ** **   ** **   **           **");
+      System.out.println(" ** **   ** **   **        ** **");
+      System.out.println(" *****   *****   **     ******* ");
+   }
+}
+
---
+++ OOPSBannerAPP3.java
@@ -0,0 +1,20 @@
+/**
+ * OOPSBannerApp
+ * UC1: Print OOPS to Console
+ * @author DIBYANSHI
+ * @version 2.0
+ */
+
+public class OOPSBannerAPP3 {   // renamed to match the filename
+
+    public static void main(String[] args) {
+        System.out.println( "***** " + " " + " ***** " + " " +" ****** " + " " +"******* ");
+        System.out.println(" ** ** " + " " + " ** ** " + " " + " ** ** " + " " + " ****");
+        System.out.println(" ** ** " + " " + " ** ** " + " " + " ** ** " + " " + " ** ");
+        System.out.println(" ** ** " + " " + " ** ** " + " " + " ****** " + " " +"******* ");
+        System.out.println(" ** ** " + " " + " ** ** " + " " + " ** " + " " + "         **");
+        System.out.println(" ** ** " + " " + " ** ** " + " " + " ** " + " " + "      ** **");
+        System.out.println(" ***** " + " " + " ***** " + " " + " ** " + " " + "   ******* ");
+    }
+
+}
